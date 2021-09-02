e3031aeac77fd8e7cf66a2cf70a615ef0c92a6c5
Andreas Kjellqvist <linco95@gmail.com>
2021-09-02 23:47:59 +0200

    Session 6

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut split_string = s.split(',');
+        let name = split_string.next();
+        if name.is_none() {
+            return Person::default();
+        }
+        let name = name.unwrap().to_string();
+
+        let age = split_string.next();
+        if age.is_none() {
+            return Person::default();
+        }
+        let age = age.unwrap().parse::<usize>();
+        if name.is_empty() || split_string.next() != None {
+            return Person::default();
+        }
+        if let Ok(age) = age {
+            return Person { name, age };
+        }
+        Person::default()
     }
 }
 
